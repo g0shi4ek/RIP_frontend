145055aa40be74c4cf1fc4db99b122c9aa49e3de
g0shi4ek <dassa01122@gmail.com>
2025-10-19 00:13:49 +0300

    added docker build

--- charging-app/vite.config.ts
+++ charging-app/vite.config.ts
@@ -5,13 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // ваш бэкенд URL
+      "/api": {
+        target: "http://host.docker.internal:8080",
         changeOrigin: true,
-        secure: false,
       },
     },
+    watch: {
+        usePolling: true,
+    }, 
+    host: true,
+    strictPort: true,
+    port: 3000,
   },
-})
+});
